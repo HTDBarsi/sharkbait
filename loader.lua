65284870ae9f1974cddbf124237151f61c414ac1
HTDBarsi <57287841+HTDBarsi@users.noreply.github.com>
2023-01-22 12:30:41 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if (game.PlaceId == 8908228901) then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/HTDBarsi/sharkbait/main/main.lua"))()
-elseif (game.PlaceId == 11040063484)
+elseif (game.PlaceId == 11040063484) then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/HTDBarsi/sharkbait/main/sword.lua"))()
 end
