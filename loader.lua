f2a58aab6bffb50ac937390b84f74afc0b741119
HTDBarsi <57287841+HTDBarsi@users.noreply.github.com>
2023-01-22 01:57:07 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if (game.PlaceId == 8908228901) then 
   loadstring(game:HttpGet("https://raw.githubusercontent.com/HTDBarsi/sharkbait/main/main.lua"))()
-else
-  print("Skill issue")
+elseif (game.PlaceId == 11040063484)
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/HTDBarsi/sharkbait/main/sword.lua"))()
 end
